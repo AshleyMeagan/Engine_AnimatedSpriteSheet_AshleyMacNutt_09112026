f5ab99cf1e6cd831a4b9fd21b1f80e6991ca24b0
AshleyMeagan <macnutta@live.com>
2026-09-11 16:23:24 -0300

    Troubleshooting
    Back tracked and still not moving..

---
+++ Assets/Scripts/LadyDMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class BatMovement : MonoBehaviour
+{
+[SerializeField] private float moveSpeed = 5f;
+
+private Rigidbody2D rb;
+private SpriteRenderer spriteRenderer;
+private Animator animator;
+private Vector2 moveInput;
+
+private void Awake()
+{
+rb = GetComponent<Rigidbody2D>();
+spriteRenderer = GetComponent<SpriteRenderer>();
+animator = GetComponent<Animator>();
+}
+
+private void Update()
+{
+float horizontal = Input.GetAxisRaw("Horizontal");
+float vertical = Input.GetAxisRaw("Vertical");
+
+moveInput = new Vector2(horizontal, vertical).normalized;
+
+animator.SetBool("IsMoving", moveInput != Vector2.zero);
+FlipLadyD();
+}
+
+private void FixedUpdate()
+{
+rb.MovePosition(rb.position +
+moveInput * moveSpeed * Time.fixedDeltaTime);
+}
+
+private void FlipLadyD()
+{
+// This bat artwork faces left by default.
+if (moveInput.x > 0)
+spriteRenderer.flipX = true;
+else if (moveInput.x < 0)
+spriteRenderer.flipX = false;
+}
+}
